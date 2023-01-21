ea8a305aca3a6e572eb5b4416510223da2582289
Nana Pisanko <bonjozzy@gmail.com>
2023-01-21 22:31:36 +0300

    feat: resolve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length%2 === 1) {
+        return false;
+    } else {
+        let stack = [];
+        let brackets = {};
+
+        for (let i = 0; i < bracketsConfig.length; i++){
+            brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+        }
+
+        const openings = Object.keys(brackets);
+        const strArray = str.split('');
+        stack.push(strArray[0]);
+
+        for (let k = 1; k <= strArray.length; k++){
+            let currentChar = strArray[k];
+            let equalChar = brackets[strArray[k]];
+            let lastCharOfStack = brackets[stack[stack.length - 1]]
+            if(currentChar === equalChar && currentChar !== equalChar || currentChar === lastCharOfStack) {
+                stack.pop();
+            } else if (openings.includes(currentChar)) {
+                stack.push(currentChar);
+            } else {
+                return false;
+            }
+        }
+
+        return stack.length === 0;
+    }
 }
